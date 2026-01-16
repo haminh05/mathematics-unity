18ecd983ec8aaa8f9b47ce7f2388ce8677d66b0f
minhha205 <148325119+haminh05@users.noreply.github.com>
2026-01-16 19:49:35 +0700

    add demo unity project

---
+++ Assets/Script/test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hà Ngọc Minh đâyyyyyyyyy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
